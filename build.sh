f76b7ba0fea88f0d061eae1a8acc7f4a291e3e93
Maksymilian Lúmenn Stachowiak <silmelumenn@gmail.com>
2024-04-29 20:34:22 +0200

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
-git clone --depth 1 --branch v4.2.3 https://github.com/jackyzha0/quartz.git quartz
-cd ./quartz && npm install
+git clone --depth 1 --branch v4.2.3 https://github.com/jackyzha0/quartz.git quartz && cd "$_"
+npm install
 npx quartz build -d ../Content
